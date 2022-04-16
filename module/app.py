65ba8abe8852d3f8a33606fdbbb642065a63fdc9
Joseph Michael Becerra <becerra.joseph424@gmail.com>
2022-04-15 23:35:34 -0700

    import flask worked successfully

---
+++ module/app.py
@@ -0,0 +1,19 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
+@app.route('/help')
+def help():
+    return 'Frequently Asked Questions n\ import issues?: is flask properly installed'
+    # this is all still on one line, but it works!
+
+# set FLASK_APP=app.py
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# flask run
